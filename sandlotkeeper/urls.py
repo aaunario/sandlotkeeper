e6894576f13602e78a5790d87de3f9fc44f36f8b
Ayrris Aunario <aaunario@gmail.com>
2021-11-15 21:36:22 -0600

    removed regex notation on url matching

--- sandlotkeeper/urls.py
+++ sandlotkeeper/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stats/', include(stats_urls)),
-    path(r'^login/$', auth_views.auth_login, name='login'),
-    path(r'^logout/$', auth_views.auth_logout, name='logout'),
+    path('login/', auth_views.auth_login, name='login'),
+    path('logout/', auth_views.auth_logout, name='logout'),
 ]
 
